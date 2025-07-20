f78aec11ca7ded6f2b282bef2545d69e44f3fcb2
CarlitosEngineer <CarlosCalderon00110001@Outlook.com>
2025-07-20 18:16:03 -0500

    zxczxc

--- src/bin/1-Variables.rs
+++ src/bin/1-Variables.rs
@@ -2,12 +2,15 @@
 ¿Qué son las variables?
 
 R/
-- La variable es un paquete de datos que se encargan de almacenar información de forma temporal.
-- las variables se conforman en dos partes, el tipo de variable y el valor de la variable.
-- las variables se declaran con ´let´, ´let mut´, ´const´.
-- Rust llama a la declaración de variables "Binding (Vinculación)"
+* La variable es un estructura que se encarga de almacenar información de forma temporal en la memoria.
+* las variables estan formadas de tres partes.
+  + El identificador de la variable, que es el nombre de la variable.
+  + La clase de valores que puede almacenar la variable, que es el tipo de variable.
+  + El dato que almacena la variable, que es el valor de la variable.
+* las variables se declaran con ´let´, ´let mut´, ´const´.
+* En Rust, la declaración de variables se denomina “binding” (vinculación).
 
-Usa este comando para ejecutar el código: cargo run --bin 1-Variables
+Usa este comando para ejecutar este código: cargo run --bin 1-Variables
 
 Fuente consultada:
 - The Rust Programming Language, Capítulo 3.1: Variables and Mutability
@@ -15,8 +18,8 @@ Fuente consultada:
 */
 
 fn learning_variables1() {
-    let variable_1: i32 = 10; // Nombre de la varaible "variable_1", Tipo de la variable. (DATO EXPLICITO, NOT INFERIDO)
-    let variable_2 = 100; // Nombre de la varaible "variable_2", Tipo de la variable. (DATO IMPLICITO, INFERIDO)
+    let variable_1: i32 = 10; // Nombre de la variable "variable_1", Tipo de la variable. (DATO EXPLICITO, NOT INFERIDO)
+    let variable_2 = 100; // Nombre de la variable "variable_2", Tipo de la variable. (DATO IMPLICITO, INFERIDO)
     println!("La variable 1 vale: {}", variable_1);
     println!("La variable 2 vale: {}", variable_2);
 }
--- src/bin/2-Constants_Mutability.rs
+++ src/bin/2-Constants_Mutability.rs
@@ -2,8 +2,10 @@
 ¿Que es la Mutabilidad?
 
 R/
-- La mutabilidad es la capacidad que tiene una variable de cambiar sus datos.
+- La mutabilidad es la capacidad que tiene una variable de cambiar su valor durante la ejecutacion del programa.
 - En Rust, las variables son inmutables por defecto; para hacerlas mutables se usa `mut`.
+
+Usa este comando para ejecutar este código: cargo run --bin 2-Constants_Mutability
 
 Fuente consultada:
 - The Rust Programming Language, Capítulo 3.1: Variables and Mutability
@@ -11,8 +13,8 @@ Fuente consultada:
 */
 
 fn learning_mutability1() {
-    let variable_1 = 0;
-    let mut variable_2 = 0;
+    let variable_1 = 0; // No Mutable
+    let mut variable_2 = 0; // Mutable
 
     // variable_1 += 1; // NOT OK
     variable_2 += 1; // OK
